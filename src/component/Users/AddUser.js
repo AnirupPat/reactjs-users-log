96a8f430c094eedebb300743e8bbe0d9cd359d63
Anirup Patnaik <anirup049@gmail.com>
2021-05-26 14:11:35 +0530

    State management

--- src/component/Users/AddUser.js
+++ src/component/Users/AddUser.js
@@ -1,20 +1,32 @@
+import { useState } from 'react';
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredUserAge, setEnteredUserAge] = useState();
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredUserAge);
   };
+
+  const userNameChangeHandler = event => {
+    setEnteredUsername(event.target.value);
+  }
+
+  const ageChangeHandler = event => {
+    setEnteredUserAge(event.target.value);
+  }
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={userNameChangeHandler} />
 
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
 
         <Button type="submit">Add User</Button>
       </form>
